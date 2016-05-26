8086d599fce431857ea8a8cbf27e11ecfc93bf4c
Julen Pardo <julen.pardo@alumni.mondragon.edu>
2016-05-26 15:24:18 +0200

    MOODLE_31_STABLE: v1.6 release

--- version.php
+++ version.php
@@ -25,8 +25,8 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->version      = 2016052600;         // The current plugin version (Date: YYYYMMDDXX).
-$plugin->release      = 'master - Release v1.6 (Build 2016052600) for Moodle 2.9, 3.0 and 3.1';
-$plugin->requires     = 2015051100;         // Required Moodle version.
+$plugin->release      = 'v3.1.1.6 (version 1.6 for Moodle 3.1) (Build 2016052600)';
+$plugin->requires     = 2016052300;         // Required Moodle version.
 $plugin->component    = 'atto_multilang2'; // Full name of the plugin (used for diagnostics).
 $plugin->maturity     = MATURITY_STABLE;
 $plugin->dependencies = array(
