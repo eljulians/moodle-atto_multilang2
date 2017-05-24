cd203953d65078fe1bf9974634bd791bdf6c91d4
Julen Pardo <julen.pardo@outlook.es>
2017-05-24 16:40:08 +0200

    Release v1.7 for Moodle 3.3

--- version.php
+++ version.php
@@ -24,9 +24,9 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version      = 2016121100;         // The current plugin version (Date: YYYYMMDDXX).
-$plugin->release      = 'master - Release v1.7 (Build 2016052600) for Moodle 2.9, 3.0, 3.1 and 3.2.';
-$plugin->requires     = 2015051100;         // Required Moodle version.
+$plugin->version      = 2017052400;         // The current plugin version (Date: YYYYMMDDXX).
+$plugin->release      = 'v3.3.1.7 (version v1.7 for Moodle 3.3) (2016121100)';
+$plugin->requires     = 2017051500;         // Required Moodle version.
 $plugin->component    = 'atto_multilang2'; // Full name of the plugin (used for diagnostics).
 $plugin->maturity     = MATURITY_STABLE;
 $plugin->dependencies = array(
