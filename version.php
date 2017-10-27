c2afda030204e9f4d063df07e08a150bc9769823
Iñaki Arenaza <iarenaza@mondragon.edu>
2017-10-27 22:17:31 +0200

    Revert release data merged by mistake

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->version      = 2017100200;         // The current plugin version (Date: YYYYMMDDXX).
-$plugin->release      = 'master - Release v1.8 (Build 2017100200) for Moodle 2.9 or later.';
+$plugin->release      = 'v3.3.1.8 (version v1.8 for Moodle 3.3) (2017100200)';
 $plugin->requires     = 2015051100;        // Required Moodle version.
 $plugin->component    = 'atto_multilang2'; // Full name of the plugin (used for diagnostics).
 $plugin->maturity     = MATURITY_STABLE;
